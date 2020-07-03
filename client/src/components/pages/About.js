31d8d7c7004adfa4255d803161f1e533f65752e4
Chinmay Parab <43921132+chinmayparab@users.noreply.github.com>
2020-07-03 19:23:08 +0530

    Base Pages

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p className='my-1'>Its a Contact Manager MERN APP.</p>
+      <p className='bg-pri p'>
+        <strong>Version 82</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
