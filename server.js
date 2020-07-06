620acf743f3e1f67c2cb017c59c095303097da28
Chinmay Parab <43921132+chinmayparab@users.noreply.github.com>
2020-07-07 02:10:57 +0530

    Fin

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -12,13 +13,20 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => res.json({ msg: "Hey there!" }));
-
 // routes
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// check if prod
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
